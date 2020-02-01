805fbb837906d7b0c8e46aed727b3f0bd31f09ee
David Bernard <david.bernard.31@gmail.com>
2020-02-01 14:15:11 +0100

    :zap: (test) cache the ubuntu + curl docker image to avoid re-install of curl at each run of test_first_run.sh

--- tests/run_ubuntu_16.sh
+++ tests/run_ubuntu_16.sh
@@ -2,11 +2,12 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+docker build -t ubuntu_curl:16.04 -f "${DIR}/ubuntu_16.dockerfile" "${DIR}"
 docker run --rm \
   -v ${DIR}/..:/prj \
   -v $HOME/.kube:/root/.kube \
   -w /tmp \
-  ubuntu:16.04 \
+  ubuntu_curl:16.04 \
   /prj/tests/test_first_run.sh
 
 # docker run --rm -it \
