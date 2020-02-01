6128dfacb8ba6d2debdda7e6a0196cb713e07f33
David Bernard <david.bernard.31@gmail.com>
2020-02-01 14:15:11 +0100

    :zap: (test) cache the ubuntu + curl docker image to avoid re-install of curl at each run of test_first_run.sh

--- tests/test_first_run.sh
+++ tests/test_first_run.sh
@@ -2,7 +2,7 @@
 
 #set -ex
 
-apt-get update && apt-get install -y curl
+# apt-get update && apt-get install -y curl
 #curl https://raw.githubusercontent.com/davidB/kubectl-view-allocations/master/scripts/getLatest.sh | sh
 #sh /prj/scripts/getLatest.sh
 bash /prj/scripts/getLatest.sh
