fe3b8af4fc6cdbbf6e78f0d266273b87102b8f98
David Bernard <david.bernard.31@gmail.com>
2021-08-26 23:51:14 +0200

    :bento: add a sample to test api<pod>.list

---
+++ examples/pod_api.rs
@@ -0,0 +1,22 @@
+use k8s_openapi::api::core::v1::Pod;
+
+use kube::{
+    api::{Api, ListParams},
+    Client,
+};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    std::env::set_var("RUST_LOG", "info,kube=debug");
+    tracing_subscriber::fmt::init();
+    let client = Client::try_default().await?;
+    let pods: Api<Pod> = Api::all(client);
+    // let pods: Api<Pod> = Api::namespaced(client, "kube-system");
+
+    let lp = ListParams::default().timeout(10);
+    let pods = pods.list(&lp).await?;
+
+    eprintln!("pods: {:?}", pods);
+
+    Ok(())
+}
